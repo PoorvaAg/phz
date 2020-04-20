7f0ba094a8cade16ac1695976ae3e924ba09b85f
PoorvaAg <agrawal.poorva40@gmail.com>
2020-04-20 19:25:56 -0400

    corrected wrong pose error in wp publisher

--- src/viz.cpp
+++ src/viz.cpp
@@ -1,29 +1,33 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
-//#include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/Quaternion.h>
 //#include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Point.h>
 #include <cmath>
 #include <vector>
+#include <tuple>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 //#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 
-#define AVERAGE_PRED 1
-
+/*
 //Global Variables
+
+
 double hw = 2.0;
 double fl = 4.0;
 double bl = 1.0;
 
-std::vector<double> gt_coords (3, 0.0);
-std::vector<double> pred_coords (3, 0.0);
+std::vector<std::tuple<double, double, geometry_msgs::Quaternion>> gt_coords;
+std::vector<std::tuple<double, double, geometry_msgs::Quaternion>> pred_coords;
+std::tuple<double, double, geometry_msgs::Quaternion> phz_coords;
 
-int count = 0;
 int flag_pred = 0;
 int flag_gt = 0;
+int flag_phz = 0;
 
 //Functions
 
@@ -77,63 +81,132 @@ visualization_msgs::Marker getBBMarker(std::string name, int id, float scale, fl
 	return mk;
 }
 
-std::vector<visualization_msgs::Marker> getBBs(){
+
+visualization_msgs::Marker getArrowMarker(std::string name, int id, float r, float x, float y, geometry_msgs::Quaternion q){
+	
+	visualization_msgs::Marker mk;
+	mk.header.frame_id = "map";
+	mk.header.stamp = ros::Time::now();
+	mk.ns = name;
+	mk.id = id;
+	mk.type = mk.ARROW;
+	mk.action = mk.ADD;
+	mk.scale.x = 0.5;
+	mk.scale.y = 0.05;
+	mk.scale.x = 0.1;
+	mk.color.a = 1.0;
+	mk.color.r = r;
+	mk.color.g = 1.0;
+	mk.color.b = 0.0;
+	mk.pose.orientation = q;
+    mk.pose.position.x = x;
+    mk.pose.position.y = y;
+    mk.pose.position.z = 0;
+
+	return mk;
+}
+
+std::vector<visualization_msgs::Marker> getMarkers(){
 
 	std::vector<visualization_msgs::Marker> v;
 
-	//for ground truth pod position
+	int id = 0;
+	//for ground truth waypoint markers
 	if (flag_gt){
-		visualization_msgs::Marker mk1 = getBBMarker("PHZ_gt", 1, 0.3, 1.0);
-		mk1.points = generate_box_corners(gt_coords[0], gt_coords[1], gt_coords[2]);		
-		v.push_back(mk1);
+
+		int n = gt_coords.size();
+		if (n>0){
+			
+			visualization_msgs::Marker mk = getBBMarker("PHZ_gt", id, 0.3, 1.0);
+			mk.points = generate_box_corners(std::get<0>(gt_coords[n-1]), std::get<1>(gt_coords[n-1]),std::get<2>(gt_coords[n-1]));
+			id++;
+			v.push_back(mk);
+
+			for (int i = 0; i<n; i++){
+				v.push_back(getArrowMarker("ground truth", id, 1.0, std::get<0>(gt_coords[n-1]), std::get<1>(gt_coords[n-1]),std::get<2>(gt_coords[n-1])));
+				id++;
+			}
+		}
+
 		flag_gt = 0;
 	}
 	
-	//for predicted pod position
+	//for predicted waypoint markers
 	if (flag_pred){
-		visualization_msgs::Marker mk2 = getBBMarker("PHZ_pred", 0, 0.2, 0.0);
-		mk2.points = generate_box_corners(pred_coords[0], pred_coords[1], pred_coords[2]);
-		v.push_back(mk2);
+		int n = pred_coords.size();
+		if (n>0){
+			
+			visualization_msgs::Marker mk = getBBMarker("PHZ_pred", id, 0.3, 0.0);
+			mk.points = generate_box_corners(std::get<0>(pred_coords[n-1]), std::get<1>(pred_coords[n-1]),std::get<2>(pred_coords[n-1]));
+			id++;
+			v.push_back(mk);
+
+			for (int i = 0; i<n; i++){
+				v.push_back(getArrowMarker("predicted", id, 0.0, std::get<0>(pred_coords[n-1]), std::get<1>(pred_coords[n-1]),std::get<2>(pred_coords[n-1])));
+				id++;
+			}
+		}
+
+		flag_pred = 0;
+	}
+
+	//for phz start location
+	if (flag_phz){
+		int n = pred_coords.size();
+		if (n>0){
+			
+			visualization_msgs::Marker mk = getBBMarker("PHZ_pred", id, 0.3, 0.0);
+			mk.points = generate_box_corners(std::get<0>(pred_coords[n-1]), std::get<1>(pred_coords[n-1]),std::get<2>(pred_coords[n-1]));
+			id++;
+			v.push_back(mk);
+
+			for (int i = 0; i<n; i++){
+				v.push_back(getArrowMarker("predicted", id, 0.0, std::get<0>(pred_coords[n-1]), std::get<1>(pred_coords[n-1]),std::get<2>(pred_coords[n-1])));
+				id++;
+			}
+		}
+
 		flag_pred = 0;
 	}
 
 	return v;
 }
 
-std::vector<double> set_params(const geometry_msgs::PoseStamped::ConstPtr& msg){
-	std::vector<double> v (3, 0.0);
-	v[0] = msg->pose.position.x;
-	v[1] = msg->pose.position.y;
+*/
 
-		tf2::Quaternion q(
-		msg->pose.orientation.x,
-		msg->pose.orientation.y,
-		msg->pose.orientation.z,
-		msg->pose.orientation.w);
-	tf2::Matrix3x3 mat(q);
+/*
+void pod_gt_CB(const geometry_msgs::PoseArray msg){
+	std::vector<std::tuple<double, double, geometry_msgs::Quaternion>> coords;
+	geometry_msgs::Pose pose;
+	for (pose : msg.poses){
+		std::tuple<double, double, geometry_msgs::Quaternion> tup(pose.position.x, pose.position.y,pose.orientation);
+		coords.push_back(tup);
+	}
 
-	double roll, pitch, yaw;
-	mat.getRPY(roll, pitch, yaw);
-	v[2] = yaw;
-
-	return v;
-}
-
-void pod_gt_CB(const geometry_msgs::PoseStamped::ConstPtr& msg){
-	gt_coords = set_params(msg);
+	gt_coords = coords;
 	flag_gt = 1;
 }
 
-void pod_pred_CB(const geometry_msgs::PoseStamped::ConstPtr& msg){
-	std::vector<double> v = set_params(msg);
-	pred_coords[0] = (pred_coords[0]*count + v[0])/(count+1);
-	pred_coords[1] = (pred_coords[1]*count + v[1])/(count+1);
-	pred_coords[2] = (pred_coords[2]*count + v[2])/(count+1);
+void pod_pred_CB(const geometry_msgs::PoseArray msg){
+	std::vector<std::tuple<double, double, geometry_msgs::Quaternion>> coords;
+	geometry_msgs::Pose pose;
+	for (pose : msg.poses){
+		std::tuple<double, double, geometry_msgs::Quaternion> tup(pose.position.x, pose.position.y,pose.orientation);
+		coords.push_back(tup);
+	}
 
-	if (AVERAGE_PRED == 1) count++;
+	pred_coords = coords;
 	flag_pred = 1;
 
 }
+
+void phz_gt_CB(const geometry_msgs::PoseStamped::ConstPtr& msg){
+	std::tuple<double, double, geometry_msgs::Quaternion> tup(pose.position.x, pose.position.y,pose.orientation);
+	phz_coords = tup;
+	flag_phz = 1;
+
+}
+*/
 
 
 int main(int argc, char **argv){
@@ -141,8 +214,10 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "marker_visualizer_node");
 	ros::NodeHandle n;	
 
-	ros::Subscriber pod_pred_sub = n.subscribe("pod_predicted_laser",1000, pod_pred_CB);
-	ros::Subscriber pod_gt_sub = n.subscribe("pod_groundtruth",1000,pod_gt_CB);
+	// ros::Subscriber pod_pred_sub = n.subscribe("waypoints_goal",1000, pod_pred_CB);
+	// ros::Subscriber pod_gt_sub = n.subscribe("gt_waypoints_goal",1000,pod_gt_CB);
+	// ros::Subscriber phz_start_sub = n.subscribe("phz_start_groundtruth",1000,phz_gt_CB);
+
 	ros::Publisher phz_pub = n.advertise<visualization_msgs::MarkerArray>("phz",10);
 	ros::Publisher phz_marker_pub = n.advertise<visualization_msgs::MarkerArray>("estimateLoc", 10);
 
@@ -151,11 +226,14 @@ int main(int argc, char **argv){
 	ros::Rate loop_rate(10);
 
 	while(ros::ok()) {
-		if (flag_pred || flag_gt){		
-			phz_array.markers = getBBs();
+
+		/*
+		if (flag_pred || flag_gt || flag_phz ){		
+			phz_array.markers = getMarkers();
 			phz_pub.publish(phz_array);
 		}
 		phz_pub.publish(phz_array);
+		*/
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
--- src/waypoint_publisher.cpp
+++ src/waypoint_publisher.cpp
@@ -1,9 +1,11 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/Pose.h>
 #include <cmath>
 #include <vector>
+#include <tuple>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
@@ -11,8 +13,9 @@
 #define RAD2DEG 57.295779513
 #define AVERAGE_PRED 1
 
-std::vector<double> gt_coords (3, 0.0);
-std::vector<double> pred_coords (3, 0.0);
+std::tuple<double, double, geometry_msgs::Quaternion> gt_coords;
+std::tuple<double, double, geometry_msgs::Quaternion> pred_coords;
+std::vector<double> last_pred (3,0);
 
 int count = 0;
 int flag_pred = 0;
@@ -21,17 +24,39 @@ int flag_gt = 0;
 
 //functions
 
-geometry_msgs::Pose set_pose(double R, double theta){
-	geometry_msgs::Pose p;
-	tf2::Quaternion quat;
-	quat.setRPY( 0, 0, theta);
-	p.orientation = tf2::toMsg(quat);
-	p.position.x = R*cos(theta);
-	p.position.y = R*sin(theta);
-	p.position.z = 0;
 
-	return p;
+std::vector<double> set_params(const geometry_msgs::PoseStamped::ConstPtr& msg);
+std::vector<geometry_msgs::Pose> generate_wps(double x, double y, geometry_msgs::Quaternion q);
+geometry_msgs::Pose set_pose(double R, double theta, geometry_msgs::Quaternion q);
+
+void pod_gt_CB(const geometry_msgs::PoseStamped::ConstPtr& msg){
+	std::get<0>(gt_coords) = msg->pose.position.x;
+	std::get<1>(gt_coords) = msg->pose.position.y;
+	std::get<2>(gt_coords) = msg->pose.orientation;
+
+	flag_gt = 1;
 }
+
+void pod_pred_CB(const geometry_msgs::PoseStamped::ConstPtr& msg){
+	std::vector<double> v = set_params(msg);
+	last_pred[0] = (last_pred[0]*count + v[0])/(count+1);
+	last_pred[1] = (last_pred[1]*count + v[1])/(count+1);
+	last_pred[2] = (last_pred[2]*count + v[2])/(count+1);
+
+	if (AVERAGE_PRED == 1) count++;
+
+	std::get<0>(pred_coords) = last_pred[0];
+	std::get<1>(pred_coords) = last_pred[1];
+
+	tf2::Quaternion quat;
+	quat.setRPY( 0, 0, last_pred[2]);
+
+	std::get<2>(pred_coords) = tf2::toMsg(quat);
+
+	flag_pred = 1;
+
+}
+
 
 std::vector<double> set_params(const geometry_msgs::PoseStamped::ConstPtr& msg){
 	std::vector<double> v (3, 0.0);
@@ -52,45 +77,39 @@ std::vector<double> set_params(const geometry_msgs::PoseStamped::ConstPtr& msg){
 	return v;
 }
 
-void pod_gt_CB(const geometry_msgs::PoseStamped::ConstPtr& msg){
-	gt_coords = set_params(msg);
-	flag_gt = 1;
-}
-
-void pod_pred_CB(const geometry_msgs::PoseStamped::ConstPtr& msg){
-	std::vector<double> v = set_params(msg);
-	pred_coords[0] = (pred_coords[0]*count + v[0])/(count+1);
-	pred_coords[1] = (pred_coords[1]*count + v[1])/(count+1);
-	pred_coords[2] = (pred_coords[2]*count + v[2])/(count+1);
-
-	if (AVERAGE_PRED == 1) count++;
-	flag_pred = 1;
-
-}
-
-
-std::vector<geometry_msgs::Pose> generate_wps(double x, double y, double theta){
+std::vector<geometry_msgs::Pose> generate_wps(double x, double y, geometry_msgs::Quaternion q){
 	
 	std::vector<geometry_msgs::Pose> poses;
-
+	double theta = atan2(y, x);
 	double R =  sqrt(x*x + y*y);
 
-	poses.push_back(set_pose(R-3.5, theta));
-	poses.push_back(set_pose(R-3.0, theta));
-	poses.push_back(set_pose(R-2.0, theta));
-	poses.push_back(set_pose(R-1.0, theta));
-	poses.push_back(set_pose(R, theta));
+	poses.push_back(set_pose(R-3.5, theta, q));
+	poses.push_back(set_pose(R-3.0, theta, q));
+	poses.push_back(set_pose(R-2.0, theta, q ));
+	poses.push_back(set_pose(R-1.0, theta, q));
+	poses.push_back(set_pose(R, theta, q));
 
 	return poses;
 }
+
+geometry_msgs::Pose set_pose(double R, double theta, geometry_msgs::Quaternion q){
+	geometry_msgs::Pose p;
+	p.orientation = q;
+	p.position.x = R*cos(theta);
+	p.position.y = R*sin(theta);
+	p.position.z = 0;
+	return p;
+}
+
 
 int main(int argc, char **argv){
 
 	ros::init(argc, argv, "waypoint_publisher_node");
 	ros::NodeHandle n;	
 
-	ros::Subscriber pod_pred_sub = n.subscribe("pod_predicted_laser",1000,pod_pred_CB);
+	ros::Subscriber pod_pred_sub = n.subscribe("pod_predicted_laser",1000, pod_pred_CB);
 	ros::Subscriber pod_gt_sub = n.subscribe("pod_groundtruth",1000,pod_gt_CB);
+
 	ros::Publisher wp_pred_pub = n.advertise<geometry_msgs::PoseArray>("waypoints_goal",10);
 	ros::Publisher wp_gt_pub = n.advertise<geometry_msgs::PoseArray>("gt_waypoints_goal",10);
 
@@ -103,23 +122,24 @@ int main(int argc, char **argv){
 		if (flag_pred){
 			wp_pred.header.stamp = ros::Time::now();
 			wp_pred.header.frame_id = "map";
-			wp_pred.poses = generate_wps(pred_coords[0],pred_coords[1],pred_coords[2]);
+			wp_pred.poses = generate_wps(std::get<0>(pred_coords),std::get<1>(pred_coords),std::get<2>(pred_coords));
 			wp_pred_pub.publish(wp_pred);
 			flag_pred = 0;
-			
-			ROS_INFO("Yaw: %.2f", pred_coords[2]*RAD2DEG);
-			ROS_INFO("WP_0: X: %.2f, Y: %.2f, Yaw:%.2f", wp_pred.poses[0].position.x, wp_pred.poses[0].position.y);
-			ROS_INFO("WP_1: X: %.2f, Y: %.2f, Yaw:%.2f", wp_pred.poses[1].position.x, wp_pred.poses[1].position.y);
-			ROS_INFO("WP_2: X: %.2f, Y: %.2f, Yaw:%.2f", wp_pred.poses[2].position.x, wp_pred.poses[2].position.y);
-			ROS_INFO("WP_3: X: %.2f, Y: %.2f, Yaw:%.2f", wp_pred.poses[3].position.x, wp_pred.poses[3].position.y);
-			ROS_INFO("WP_4: X: %.2f, Y: %.2f, Yaw:%.2f", wp_pred.poses[4].position.x, wp_pred.poses[4].position.y);
 
+			
+			ROS_INFO("Yaw: %.2f", last_pred[2]*RAD2DEG);
+			ROS_INFO("WP_0: X: %.2f, Y: %.2f", wp_pred.poses[0].position.x, wp_pred.poses[0].position.y);
+			ROS_INFO("WP_1: X: %.2f, Y: %.2f", wp_pred.poses[1].position.x, wp_pred.poses[1].position.y);
+			ROS_INFO("WP_2: X: %.2f, Y: %.2f", wp_pred.poses[2].position.x, wp_pred.poses[2].position.y);
+			ROS_INFO("WP_3: X: %.2f, Y: %.2f", wp_pred.poses[3].position.x, wp_pred.poses[3].position.y);
+			ROS_INFO("WP_4: X: %.2f, Y: %.2f", wp_pred.poses[4].position.x, wp_pred.poses[4].position.y);
+			
 		}
 
 		if (flag_gt){
 			wp_gt.header.stamp = ros::Time::now();
 			wp_gt.header.frame_id = "map";
-			wp_gt.poses = generate_wps(gt_coords[0],gt_coords[1],gt_coords[2]);
+			wp_gt.poses = generate_wps(std::get<0>(gt_coords),std::get<1>(gt_coords),std::get<2>(gt_coords));
 			wp_gt_pub.publish(wp_gt);
 			flag_gt = 0;
 		}
