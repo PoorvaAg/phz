f48d9a520f80f789fcc55029955effad8be09ed6
PoorvaAg <agrawal.poorva40@gmail.com>
2020-04-21 02:06:16 -0400

    viz_node complete

--- src/viz.cpp
+++ src/viz.cpp
@@ -13,33 +13,33 @@
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 
+#define PI 3.14159
+
 
 //Global Variables
-
-
+//limits of phz around pod location
 double hw = 2.0;
 double fl = 4.0;
 double bl = 1.0;
 
-std::vector<std::tuple<double, double, geometry_msgs::Quaternion>> gt_coords;
+//to store phz waypoints
 std::vector<std::tuple<double, double, geometry_msgs::Quaternion>> pred_coords;
+std::vector<std::tuple<double, double, geometry_msgs::Quaternion>> gt_coords;
 std::tuple<double, double, geometry_msgs::Quaternion> phz_coords;
 
+//callback flags
 int flag_pred = 0;
 int flag_gt = 0;
 int flag_phz = 0;
 
 //Functions
-
 //Subscriber Callbacks
 void pod_pred_CB(const geometry_msgs::PoseArray msg){
 	std::vector<std::tuple<double, double, geometry_msgs::Quaternion>> coords;
-	geometry_msgs::Pose pose;
-	for (pose : msg.poses){
+	for (geometry_msgs::Pose pose : msg.poses){
 		std::tuple<double, double, geometry_msgs::Quaternion> tup(pose.position.x, pose.position.y,pose.orientation);
 		coords.push_back(tup);
 	}
-
 	pred_coords = coords;
 	flag_pred = 1;
 
@@ -47,48 +47,55 @@ void pod_pred_CB(const geometry_msgs::PoseArray msg){
 
 void pod_gt_CB(const geometry_msgs::PoseArray msg){
 	std::vector<std::tuple<double, double, geometry_msgs::Quaternion>> coords;
-	geometry_msgs::Pose pose;
-	for (pose : msg.poses){
+	for (geometry_msgs::Pose pose : msg.poses){
 		std::tuple<double, double, geometry_msgs::Quaternion> tup(pose.position.x, pose.position.y,pose.orientation);
 		coords.push_back(tup);
 	}
-
 	gt_coords = coords;
 	flag_gt = 1;
 }
 
 
-void phz_gt_CB(const geometry_msgs::PoseStamped::ConstPtr& msg){
-	std::tuple<double, double, geometry_msgs::Quaternion> tup(pose.position.x, pose.position.y,pose.orientation);
+void phz_gt_CB(const geometry_msgs::PoseStamped msg){
+	std::tuple<double, double, geometry_msgs::Quaternion> tup(msg.pose.position.x, msg.pose.position.y,msg.pose.orientation);
 	phz_coords = tup;
 	flag_phz = 1;
-
 }
 
-
-
-std::vector<geometry_msgs::Point> generate_box_corners(double x, double y, double theta){
+std::vector<geometry_msgs::Point> generate_box_corners(double x, double y, geometry_msgs::Quaternion q){
 	std::vector<geometry_msgs::Point> pts;
-	double R = sqrt(x*x+y*y);
+
+	tf2::Quaternion quat(q.x, q.y, q.z, q.w);
+	tf2::Matrix3x3 mat(quat);
+	double roll, pitch, theta;
+	mat.getRPY(roll, pitch, theta);
+
+	double beta = PI/2 - theta;
+
+	double sx = x - fl*cos(theta);
+	double sy = y - fl*sin(theta);
+	double ex = x + bl*cos(theta);
+	double ey = y + bl*sin(theta);
+	
 
 	geometry_msgs::Point ll;
-	ll.x = (R-fl)*cos(theta) - hw*sin(theta);
-	ll.y = (R-fl)*sin(theta) + hw*cos(theta);
+	ll.x = sx - hw*cos(beta);
+	ll.y = sy + hw*sin(beta);
 	ll.z = 0.0;
 	
 	geometry_msgs::Point lr;
-	lr.x = (R-fl)*cos(theta) - (-hw)*sin(theta);
-	lr.y = (R-fl)*sin(theta) + (-hw)*cos(theta);
+	lr.x = sx + hw*cos(beta);
+	lr.y = sy - hw*sin(beta);
 	lr.z = 0.0;
 
 	geometry_msgs::Point ul;
-	ul.x = (R+bl)*cos(theta) - hw*sin(theta);
-	ul.y = (R+bl)*sin(theta) + hw*cos(theta);
+	ul.x = ex + hw*cos(beta);
+	ul.y = ey - hw*sin(beta);
 	ul.z = 0.0;
 
 	geometry_msgs::Point ur;
-	ur.x = (R+bl)*cos(theta) - (-hw)*sin(theta);
-	ur.y = (R+bl)*sin(theta) + (-hw)*cos(theta);
+	ur.x = ex - hw*cos(beta);
+	ur.y = ey + hw*sin(beta);
 	ur.z = 0.0;
 
 	pts.push_back(ll);
@@ -118,7 +125,7 @@ visualization_msgs::Marker getBBMarker(std::string name, int id, float scale, fl
 }
 
 
-visualization_msgs::Marker getArrowMarker(std::string name, int id, float r, float x, float y, geometry_msgs::Quaternion q){
+visualization_msgs::Marker getArrowMarker(std::string name, int id, float r, double x, double y, geometry_msgs::Quaternion q){
 	
 	visualization_msgs::Marker mk;
 	mk.header.frame_id = "map";
@@ -153,13 +160,13 @@ std::vector<visualization_msgs::Marker> getMarkers(){
 		int n = gt_coords.size();
 		if (n>0){
 			
-			visualization_msgs::Marker mk = getBBMarker("PHZ_gt", id, 0.3, 1.0);
+			visualization_msgs::Marker mk = getBBMarker("PHZ_ground_truth", id, 0.3, 1.0);
 			mk.points = generate_box_corners(std::get<0>(gt_coords[n-1]), std::get<1>(gt_coords[n-1]),std::get<2>(gt_coords[n-1]));
 			id++;
 			v.push_back(mk);
 
 			for (int i = 0; i<n; i++){
-				v.push_back(getArrowMarker("ground truth", id, 1.0, std::get<0>(gt_coords[n-1]), std::get<1>(gt_coords[n-1]),std::get<2>(gt_coords[n-1])));
+				v.push_back(getArrowMarker("ground_truth", id, 1.0, std::get<0>(gt_coords[n-1]), std::get<1>(gt_coords[n-1]),std::get<2>(gt_coords[n-1])));
 				id++;
 			}
 		}
@@ -172,7 +179,7 @@ std::vector<visualization_msgs::Marker> getMarkers(){
 		int n = pred_coords.size();
 		if (n>0){
 			
-			visualization_msgs::Marker mk = getBBMarker("PHZ_pred", id, 0.3, 0.0);
+			visualization_msgs::Marker mk = getBBMarker("PHZ_predicted", id, 0.3, 0.0);
 			mk.points = generate_box_corners(std::get<0>(pred_coords[n-1]), std::get<1>(pred_coords[n-1]),std::get<2>(pred_coords[n-1]));
 			id++;
 			v.push_back(mk);
@@ -188,32 +195,12 @@ std::vector<visualization_msgs::Marker> getMarkers(){
 
 	//for phz start location
 	if (flag_phz){
-		int n = pred_coords.size();
-		if (n>0){
-			
-			visualization_msgs::Marker mk = getBBMarker("PHZ_pred", id, 0.3, 0.0);
-			mk.points = generate_box_corners(std::get<0>(pred_coords[n-1]), std::get<1>(pred_coords[n-1]),std::get<2>(pred_coords[n-1]));
-			id++;
-			v.push_back(mk);
-
-			for (int i = 0; i<n; i++){
-				v.push_back(getArrowMarker("predicted", id, 0.0, std::get<0>(pred_coords[n-1]), std::get<1>(pred_coords[n-1]),std::get<2>(pred_coords[n-1])));
-				id++;
-			}
-		}
-
-		flag_pred = 0;
+		v.push_back(getArrowMarker("PHZ_start", id, 0.0, std::get<0>(phz_coords), std::get<1>(phz_coords),std::get<2>(phz_coords)));
+		flag_phz = 0;
 	}
 
 	return v;
 }
-
-
-
-
-
-
-
 
 int main(int argc, char **argv){
 
@@ -237,7 +224,6 @@ int main(int argc, char **argv){
 			phz_array.markers = getMarkers();
 			phz_pub.publish(phz_array);
 		}
-		phz_pub.publish(phz_array);
 		
 		ros::spinOnce();
 		loop_rate.sleep();
